9756c50d44fd48327d4c40f8bf6a1b07462ffa01
sefa <113290365+absosec@users.noreply.github.com>
2024-05-06 01:14:09 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,11 @@
+fetch("https://rc7b8x4qmcbnuq3ghbam5ro7lyrpff34.oastify.com/test", {
+  method: "POST",
+  body: JSON.stringify({
+    userId: 1,
+    title: "Fix my bugs",
+    completed: false
+  }),
+  headers: {
+    "Content-type": "application/json; charset=UTF-8"
+  }
+});
